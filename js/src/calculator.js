32ffb04fed07f5ddd3f95c6d03b2a2eb45cb5dee
James Pederson <james@jpederson.com>
2014-05-22 15:51:43 -0500

    Fixed results table display with overflow-x: auto

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -6,12 +6,12 @@ $(function(){
 		mode: "compare",
 		response_output_div: ".result-breakdown",
 		response_compare:'<h2 class="large-title">Congratulations!<br>You can save up to <span>$%savings%</span> in interest!</h2>'+
-			'<table cellspacing="0" border=0>'+
+			'<div class="result-table"><table cellspacing="0" border=0>'+
 				'<tr><th></th><th>Current Loan</th><th>Henrico FCU</th></tr>'+
 				'<tr><td>Monthly Payment</td><td class="strike">$%b_payment_amount%</td><td class="green">$%a_payment_amount%</td></tr>'+
 				'<tr><td>Total Payments</td><td class="strike">$%b_total_payments%</td><td class="green">$%a_total_payments%</td></tr>'+
 				'<tr><td>Total Interest</td><td class="strike">$%b_total_interest%</td><td class="green">$%a_total_interest%</td></tr>'+
-			'</table',
+			'</table></div>',
 		button_label: "Calculate My Savings",
 		operation: "button",
 		error_text: "$0",
